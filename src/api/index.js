7dfc416698796fa745660de9c465d1dfa9e1f6e2
albertjarett <128996701+albertjarett@users.noreply.github.com>
2026-01-01 20:50:18 +0100

    Implement anime search API endpoint
    Added an API endpoint to search for anime using web scraping.

--- src/api/index.js
+++ src/api/index.js
@@ -1,2 +1,29 @@
-const app = require('../src/api/index'); 
+const express = require('express');
+const axios = require('axios');
+const cheerio = require('cheerio');
+const cors = require('cors');
+const app = express();
+
+app.use(cors());
+
+// Ruta para que tu Android busque animes
+app.get('/api/search', async (req, res) => {
+  const query = req.query.query;
+  try {
+    const { data } = await axios.get(`https://www3.animeflv.net/browse?q=${query}`);
+    const $ = cheerio.load(data);
+    const results = [];
+    $('.ListAnimes li').each((i, el) => {
+      results.push({
+        title: $(el).find('.Title').text(),
+        id: $(el).find('a').attr('href').replace('/anime/', ''),
+        poster: $(el).find('img').attr('src')
+      });
+    });
+    res.json(results);
+  } catch (e) {
+    res.status(500).json({ error: "Error en el servidor" });
+  }
+});
+
 module.exports = app;
